83082361e64df925abc8e115798c05fc8b8223c5
Devesh Dipak Jadhav <dj64360@hmai.com>
2024-07-04 18:57:32 +0530

    open weather api first commit

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ import dotenv from "dotenv";
 // Load environment variables from .env file
 dotenv.config();
 
-// Log environment variables to debug
-// console.log("API_KEY:", process.env.API_KEY);
-// console.log("PORT:", process.env.PORT);
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -16,11 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// API-key
-const yourBearerToken = process.env.API_KEY;
-// console.log("Bearer Token:", yourBearerToken);
-
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${yourBearerToken}&units=metric`;
+const API_KEY = process.env.API_KEY;
 
 // Define routes
 app.get("/", (req, res) => {
@@ -31,13 +23,10 @@ app.post("/getWeather", async (req, res) => {
   const { latitude, longitude } = req.body;
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${yourBearerToken}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
     const weatherData = response.data;
-    // console.log(weatherData.main.temp);
-
-    // You can send the weather data to the client
-    res.render("index.ejs", { weather: weatherData });
+    res.render("home.ejs", { weather: weatherData });
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while fetching the weather data.");
